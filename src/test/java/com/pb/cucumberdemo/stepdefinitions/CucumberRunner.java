afa6c992fc2840f197afda9fe15742b02eeb75b5
Ari <74683543+gamrdrm@users.noreply.github.com>
2022-10-26 09:27:42 +0530

    Update CucumberRunner.java

--- src/test/java/com/pb/cucumberdemo/stepdefinitions/CucumberRunner.java
+++ src/test/java/com/pb/cucumberdemo/stepdefinitions/CucumberRunner.java
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin = {"pretty", "html:target/cucumber"},
+		
+		plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
 		features = {"src/test/resources/features"},
 		glue={"com.pb.cucumberdemo.stepdefinitions"},
 		monochrome = true,
