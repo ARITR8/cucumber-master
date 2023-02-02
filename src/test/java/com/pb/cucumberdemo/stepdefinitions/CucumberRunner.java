4d9a2af49af5f8688f21ac3f90bf4feee73a64c2
Ari <aritradas75@gmail.com>
2023-02-02 15:04:45 +0530

    ad
    asd

--- src/test/java/com/pb/cucumberdemo/stepdefinitions/CucumberRunner.java
+++ src/test/java/com/pb/cucumberdemo/stepdefinitions/CucumberRunner.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
 		features = {"src/test/resources/features"},
 		glue={"com.pb.cucumberdemo.stepdefinitions"},
 		monochrome = true,
-		tags = {"@userflow"},
+		tags = {"@tag"},
 		dryRun = false,
 		strict = true
 		)
